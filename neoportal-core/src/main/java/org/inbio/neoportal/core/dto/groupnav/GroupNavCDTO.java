29c4b61431b5bfca3a0acd804eeb35a67a130656
Arturo Vargas <turo.vargas@gmail.com>
2012-07-03 17:42:02 -0600

    Add GroupNavDAO and GroupNavCDTO

---
+++ neoportal-core/src/main/java/org/inbio/neoportal/core/dto/groupnav/GroupNavCDTO.java
@@ -0,0 +1,100 @@
+package org.inbio.neoportal.core.dto.groupnav;
+
+import java.util.List;
+
+import org.inbio.neoportal.common.dto.BaseDTO;
+
+/**
+ * @author avargas
+ *
+ * DTO for GroupNav, because this is for use 
+ * after closing database call, the property groupNavChilds
+ * only keep one level (groupNavChilds of a child would be null).
+ */
+public class GroupNavCDTO extends BaseDTO
+		implements Comparable{
+	
+	private String groupNavId;
+	private String groupNavParentId;
+	private String taxonId;
+	private List<GroupNavCDTO> groupNavChilds;
+
+	@Override
+	public int compareTo(Object o) {
+		return groupNavId.compareTo(((GroupNavCDTO)o).getGroupNavId());
+	}
+	
+	public GroupNavCDTO() {
+		super();
+	}
+
+	
+	public GroupNavCDTO(String groupNavId, String groupNavParentId,
+			String taxonId, List<GroupNavCDTO> groupNavChilds) {
+		super();
+		this.groupNavId = groupNavId;
+		this.groupNavParentId = groupNavParentId;
+		this.taxonId = taxonId;
+		this.groupNavChilds = groupNavChilds;
+	}
+
+	/**
+	 * @return the groupNavId
+	 */
+	public String getGroupNavId() {
+		return groupNavId;
+	}
+
+	/**
+	 * @param groupNavId the groupNavId to set
+	 */
+	public void setGroupNavId(String groupNavId) {
+		this.groupNavId = groupNavId;
+	}
+
+	/**
+	 * @return the groupNavParentId
+	 */
+	public String getGroupNavParentId() {
+		return groupNavParentId;
+	}
+
+	/**
+	 * @param groupNavParentId the groupNavParentId to set
+	 */
+	public void setGroupNavParentId(String groupNavParentId) {
+		this.groupNavParentId = groupNavParentId;
+	}
+
+	/**
+	 * @return the taxonId
+	 */
+	public String getTaxonId() {
+		return taxonId;
+	}
+
+	/**
+	 * @param taxonId the taxonId to set
+	 */
+	public void setTaxonId(String taxonId) {
+		this.taxonId = taxonId;
+	}
+
+	/**
+	 * @return the groupNavChilds
+	 */
+	public List<GroupNavCDTO> getGroupNavChilds() {
+		return groupNavChilds;
+	}
+
+	/**
+	 * @param groupNavChilds the groupNavChilds to set
+	 */
+	public void setGroupNavChilds(List<GroupNavCDTO> groupNavChilds) {
+		this.groupNavChilds = groupNavChilds;
+	}
+	
+
+	
+	
+}
