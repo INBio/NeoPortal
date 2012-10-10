601f566bd21f45b22d669f786cefee942648a997
Arturo Vargas <turo.vargas@gmail.com>
2012-10-10 17:33:27 -0600

    Fix wrong merge

--- neoportal-core/src/main/java/org/inbio/neoportal/core/entity/GroupNav.java
+++ neoportal-core/src/main/java/org/inbio/neoportal/core/entity/GroupNav.java
@@ -11,11 +11,12 @@ import java.util.Set;
  */
 public class GroupNav implements java.io.Serializable {
 
-
      private BigDecimal groupNavId;
+     private String name;
+     private String nameEng;
      private GroupNav groupNavParent;
      private BigDecimal taxonId;
-     private Set groupNavChilds = new HashSet(0);
+     private Set<GroupNav> groupNavChilds = new HashSet<GroupNav>();
 
     public GroupNav() {
     }
@@ -24,7 +25,101 @@ public class GroupNav implements java.io.Serializable {
         this.groupNavId = groupNavId;
     }
 
+	public GroupNav(BigDecimal groupNavId, GroupNav groupNavParen, 
+			BigDecimal taxonId, Set groupNavChilds) {
+		super();
+		this.groupNavId = groupNavId;
+		this.groupNavParent = groupNavParent;
+		this.taxonId = taxonId;
+		this.groupNavChilds = groupNavChilds;
+	}
 
+	/**
+	 * @return the groupNavId
+	 */
+	public BigDecimal getGroupNavId() {
+		return groupNavId;
+	}
+
+	/**
+	 * @param groupNavId the groupNavId to set
+	 */
+	public void setGroupNavId(BigDecimal groupNavId) {
+		this.groupNavId = groupNavId;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the nameEng
+	 */
+	public String getNameEng() {
+		return nameEng;
+	}
+
+	/**
+	 * @param nameEng the nameEng to set
+	 */
+	public void setNameEng(String nameEng) {
+		this.nameEng = nameEng;
+	}
+
+	/**
+	 * @return the groupNavParent
+	 */
+	public GroupNav getGroupNavParent() {
+		return groupNavParent;
+	}
+
+	/**
+	 * @param groupNavParent the groupNavParent to set
+	 */
+	public void setGroupNavParent(GroupNav groupNavParent) {
+		this.groupNavParent = groupNavParent;
+	}
+
+	/**
+	 * @return the taxonId
+	 */
+	public BigDecimal getTaxonId() {
+		return taxonId;
+	}
+
+	/**
+	 * @param taxonId the taxonId to set
+	 */
+	public void setTaxonId(BigDecimal taxonId) {
+		this.taxonId = taxonId;
+	}
+
+	/**
+	 * @return the groupNavChilds
+	 */
+	public Set<GroupNav> getGroupNavChilds() {
+		return groupNavChilds;
+	}
+
+	/**
+	 * @param groupNavChilds the groupNavChilds to set
+	 */
+	public void setGroupNavChilds(Set<GroupNav> groupNavChilds) {
+		this.groupNavChilds = groupNavChilds;
+	}
+
+
+   
 }
 
 
